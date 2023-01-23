a4ed051c7c03f72d1e3b3deaed3be9a246666c57
Christian Westgaard <comlock@gmail.com>
2023-01-23 15:55:15 +0100

    Test aggregations

--- src/main/resources/main.ts
+++ src/main/resources/main.ts
@@ -7,22 +7,22 @@ import { execute } from '/lib/graphql';
 const Diff = require('diff');
 //@ts-ignore
 import { createSchema as createGqlSchema} from '/lib/guillotine';
-import {
-	createVirtualApplication,
-	// get as getApp,
-	getApplicationMode,
-	// list as listApps
-} from '/lib/xp/app';
+// import {
+// 	createVirtualApplication,
+// 	// get as getApp,
+// 	getApplicationMode,
+// 	// list as listApps
+// } from '/lib/xp/app';
 import { create as createContent } from '/lib/xp/content';
 import { run } from '/lib/xp/context';
 import {
 	create as createProject,
 	delete as deleteProject
 } from '/lib/xp/project';
-import {
-	createSchema,
-	listSchemas,
-} from '/lib/xp/schema';
+// import {
+// 	createSchema,
+// 	listSchemas,
+// } from '/lib/xp/schema';
 import { executeFunction } from '/lib/xp/task';
 
 // const { diff: detailedDiff } = new HumanDiff({
@@ -32,8 +32,9 @@ import { executeFunction } from '/lib/xp/task';
 const PROJECT_ID = app.name.replace('com.enonic.app.', '').replace(/\./g, '-');
 const REPO_ID = `com.enonic.cms.${PROJECT_ID}` as const;
 const BRANCH_ID = 'draft';
-const APP_KEY_VIRTUAL = `${app.name}.virtual` as const;
-const CONTENT_TYPE = `${APP_KEY_VIRTUAL}:test` as const;
+// const APP_KEY_VIRTUAL = `${app.name}.virtual` as const;
+const APP_KEY = app.name;
+const CONTENT_TYPE = `${APP_KEY}:test` as const;
 
 
 const gqlSchema = createGqlSchema();
@@ -48,21 +49,21 @@ function task() {
 			'role:system.admin',
 		]
 	}, () => {
-		try {
-			const vApp = createVirtualApplication({
-				key: APP_KEY_VIRTUAL
-			});
-			log.info('vApp:%s', toStr(vApp));
-		} catch (e) {
-			if (e.class.name !== 'com.enonic.xp.node.NodeAlreadyExistAtPathException') {
-				log.error(`e.class.name:${toStr(e.class.name)} e.message:${toStr(e.message)}`, e);
-			}
-		} // try/catch
+		// try {
+		// 	const vApp = createVirtualApplication({
+		// 		key: APP_KEY_VIRTUAL
+		// 	});
+		// 	log.info('vApp:%s', toStr(vApp));
+		// } catch (e) {
+		// 	if (e.class.name !== 'com.enonic.xp.node.NodeAlreadyExistAtPathException') {
+		// 		log.error(`e.class.name:${toStr(e.class.name)} e.message:${toStr(e.message)}`, e);
+		// 	}
+		// } // try/catch
 
-		const applicationMode = getApplicationMode({
-			key: APP_KEY_VIRTUAL
-		});
-		log.info('applicationMode:%s', applicationMode); // virtual | bundled | augmented
+		// const applicationMode = getApplicationMode({
+		// 	key: APP_KEY_VIRTUAL
+		// });
+		// log.info('applicationMode:%s', applicationMode); // virtual | bundled | augmented
 
 		// const appInfo = getApp({
 		// 	key: APP_KEY_VIRTUAL
@@ -72,38 +73,43 @@ function task() {
 		// const apps = listApps();
 		// log.info('apps:%s', toStr(apps)); // long list
 
-		try {
-			const createdSchema = createSchema({
-				name: CONTENT_TYPE,
-				type: 'CONTENT_TYPE',
-				resource: `<content-type>
-		<description>Test description</description>
-		<display-name>Test displayName</display-name>
-		<super-type>base:structured</super-type>
-		<is-abstract>false</is-abstract>
-		<is-final>true</is-final>
-		<is-built-in>false</is-built-in>
-		<allow-child-content>true</allow-child-content>
-		<form>
-			<input name="price" type="Double">
-				<label>Price</label>
-				<occurrences minimum="0" maximum="1"/>
-			</input>
-		</form>
-	</content-type>`,
-			});
-			log.info('createdSchema:%s', createdSchema);
-		} catch (e) {
-			if (e.class.name !== 'com.enonic.xp.node.NodeAlreadyExistAtPathException') {
-				log.error(`e.class.name:${toStr(e.class.name)} e.message:${toStr(e.message)}`, e);
-			}
-		} // try/catch
+// 		try {
+// 			const createdSchema = createSchema({
+// 				name: CONTENT_TYPE,
+// 				type: 'CONTENT_TYPE',
+// 				resource: `<content-type>
+// 	<description>Test description</description>
+// 	<display-name>Test displayName</display-name>
+// 	<super-type>base:structured</super-type>
+// 	<is-abstract>false</is-abstract>
+// 	<is-final>true</is-final>
+// 	<is-built-in>false</is-built-in>
+// 	<allow-child-content>true</allow-child-content>
+// 	<form>
+// 		<input name="location" type="GeoPoint">
+// 			<label>Location</label>
+// 			<occurrences minimum="1" maximum="1"/>
+// 		</input>
+// 		<input name="price" type="Double">
+// 			<label>Price</label>
+// 			<occurrences minimum="0" maximum="1"/>
+// 		</input>
+// 	</form>
+// </content-type>`,
+// 			});
+// 			log.info('createdSchema:%s', createdSchema);
+// 		} catch (e) {
+// 			if (e.class.name !== 'com.enonic.xp.node.NodeAlreadyExistAtPathException') {
+// 				log.error(`e.class.name:${toStr(e.class.name)} e.message:${toStr(e.message)}`, e);
+// 			}
+// 		} // try/catch
 
-		const schemas = listSchemas({
-			application: APP_KEY_VIRTUAL,
-			type: 'CONTENT_TYPE'
-		});
-		log.info('schemas:%s', schemas);
+// 		const schemas = listSchemas({
+// 			application: APP_KEY_VIRTUAL,
+// 			type: 'CONTENT_TYPE'
+// 		});
+// 		log.info('schemas:%s', schemas);
+
 		try {
 			deleteProject({
 				id: PROJECT_ID,
@@ -138,6 +144,7 @@ function task() {
 				//childOrder: '',
 				contentType: CONTENT_TYPE,
 				data: {
+					location: '59.91273, 10.74609', // Oslo
 					price: 1
 				},
 				displayName: 'My Folder',
@@ -164,6 +171,7 @@ function task() {
 			subFolderId = createContent({
 				contentType: CONTENT_TYPE,
 				data: {
+					location: '60.39299, 5.32415', // Bergen
 					price: -0.1
 				},
 				displayName: 'My Sub Folder',
@@ -557,6 +565,191 @@ function task() {
 			const boolEqual = fde(expected, actual);
 			log.info('boolEqual:%s', boolEqual);
 			if (!boolEqual) {
+				// log.info('diff:%s', toStr(detailedDiff(expected, actual)));
+				log.info('diff:%s', toStr(Diff.diffJson(expected, actual)));
+			}
+		} catch (e) {
+			log.error(`e.class.name:${toStr(e.class.name)} e.message:${toStr(e.message)}`, e);
+		} // try/catch
+
+		try {
+			const query = `{
+	guillotine {
+		queryDslConnection(
+			aggregations: [
+				{name: "countPrice", count: {field: "data.price"}},
+				{name: "minPrice", min: {field: "data.price"}},
+				{name: "maxPrice", max: {field: "data.price"}},
+				{name: "range", range: {
+					field: "data.price"
+					ranges: [{
+						# key: "positivePrices"
+						from: 0
+					},{
+						# key: "negativePrices"
+						to: 0
+					}]
+				}},
+				{
+					name: "type", subAggregations: [
+						{name: "priceStats", stats: {field: "data.price"}}
+					],
+					terms: {field: "type"}
+				},
+				{
+					name: "dateHistogram"
+					dateHistogram: {
+						field: "_ts"
+						interval: "1M"
+						minDocCount: 0
+						format: "MM-yyy"
+					}
+				},
+				{
+					name: "dateRange",
+					dateRange: {
+						field: "_ts"
+						format: "MM-yyy"
+						ranges: [{
+							# key: "pre23"
+							to: "01-2023"
+						},{
+							# key: "2023toEternity"
+							from: "01-2023"
+						}]
+					}
+				},
+				{
+					name: "geoDistance"
+					geoDistance: {
+						field: "data.location"
+						origin: {
+						lat: "59.91273"
+						lon: "10.74609"
+						}
+						unit: "km" # Should be enum in the future
+						ranges: [{
+						# key: "under100km"
+						to: 100
+						},{
+						# key: "above100km"
+						from: 100
+						}]
+					}
+				}
+			]
+			query: {fulltext: {fields: ["_allText", "displayName"], query: "folder", operator: OR}}
+		) {
+			aggregationsAsJson
+			edges {
+				node {
+					_path
+					dataAsJson
+					type
+				}
+			}
+		}
+	}
+}`
+			const expected = {
+				data:{
+					guillotine: {
+						queryDslConnection: {
+							aggregationsAsJson: {
+								countPrice: {
+									value: 2
+								},
+								geoDistance: {
+									buckets: [{
+										key: '*-100.0',
+										docCount: 1
+									},{
+										key: '100.0-*',
+										docCount: 1
+									}]
+								},
+								dateRange: {
+									buckets: [{
+										key: '*-01-2023',
+										docCount: 0,
+										to: '2023-01-01T00:00:00Z'
+									},{
+										key: '01-2023-*',
+										docCount: 2,
+										from: '2023-01-01T00:00:00Z'
+									}]
+								},
+								minPrice: {
+									value: -0.1
+								},
+								dateHistogram: {
+									buckets: [{
+										key: '01-2023',
+										docCount: 2
+									}]
+								},
+								range: {
+									buckets: [{
+										key: '*-0.0',
+										docCount: 1,
+										from: null,
+										to: 0
+									},{
+										key: '0.0-*',
+										docCount: 1,
+										from: 0,
+										to: null
+									}]
+								},
+								maxPrice: {
+									value: 1
+								},
+								type: {
+									buckets: [{
+										key: 'com.enonic.app.test.lib.guillotine:test',
+										docCount: 2,
+										priceStats: {
+											count: 2,
+											min: -0.1,
+											max: 1,
+											avg: 0.45,
+											sum: 0.9
+										}
+									}]
+								}
+							}, // aggregationsAsJson
+							edges: [{
+								node: {
+									_path: '/folder',
+									dataAsJson: {
+										location: '59.91273,10.74609',
+										price: 1
+									},
+									type: 'com.enonic.app.test.lib.guillotine:test'
+								}
+							},{
+								node: {
+									_path: '/folder/subFolder',
+									dataAsJson: {
+										location: '60.39299,5.32415',
+										price: -0.1
+									},
+									type: 'com.enonic.app.test.lib.guillotine:test'
+								}
+							}] // edges
+						}, // queryDslConnection
+					} // guillotine
+				} // data
+			};
+			const actual = execute(gqlSchema, query, variables, context);
+			const boolEqual = fde(
+				// stringify -> parse needed to avoid diff on date objs
+				JSON.parse(JSON.stringify(expected)),
+				JSON.parse(JSON.stringify(actual)),
+			);
+			log.info('aggregations:%s', boolEqual);
+			if (!boolEqual) {
+				log.info('actual:%s', toStr(actual));
 				// log.info('diff:%s', toStr(detailedDiff(expected, actual)));
 				log.info('diff:%s', toStr(Diff.diffJson(expected, actual)));
 			}
